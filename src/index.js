c6cd83d6c7748994cf8733210b242d6570bbe1f6
Eric JANVIER <janviereric@icloud.com>
2023-08-09 13:24:55 +0200

    Mise en place de la fonctionnalité pour éditer une Todo

--- src/index.js
+++ src/index.js
@@ -16,42 +16,23 @@ const todos = [
     text: "Je suis la Todo numéros 1",
     done: false,
     check: false,
+    editMode: false,
   },
   {
     text: "Je suis la Todo numéros 2",
-    done: true,
-    check: true,
-  },
-  {
-    text: "Je suis la Todo numéros 3",
-    done: true,
-    check: true,
-  },
-  {
-    text: "Je suis la Todo numéros 4",
-    done: true,
-    check: true,
-  },
-  {
-    text: "Je suis la Todo numéros 5",
-    done: true,
-    check: true,
-  },
-  {
-    text: "Je suis la Todo numéros 6",
-    done: true,
-    check: true,
-  },
-  {
-    text: "Je suis la Todo numéros 7",
-    done: true,
-    check: true,
+    done: false,
+    check: false,
+    editMode: false,
   },
 ];
 
 const displayTodo = () => {
   const todosElement = todos.map((todo, index) => {
-    return createTodoElement(todo, index);
+    if (todo.editMode) {
+      return createTodoEditElement(todo, index);
+    } else {
+      return createTodoElement(todo, index);
+    }
   });
   ul.innerHTML = "";
   ul.append(...todosElement);
@@ -64,16 +45,19 @@ const createTodoElement = (todo, index) => {
   <legend class="title-todo">
     Ma tâche n°<span class="number">${index + 1}</span> :
   </legend>
-  <p class="container-text-todo">
-    <span class="todo">${index + 1}</span> 
-    <span class="${todo.done ? "done" : ""}"></span> 
-    <span class="text-todo">${todo.text}</span>
+  <div class="container-done-text-todo-check">
+    <span class="${todo.done ? "done" : "todo"}"> ${
+    todo.done ? "ok" : index + 1
+  }</span> 
+    <div class="container-text-todo">
+      <p class="text-todo">${todo.text}</p>
+    </div>
     <span>${
       todo.check
         ? "<i class='fa-regular fa-square-check'></i>"
         : "<i class='fa-regular fa-square'></i>"
     }</span>
-  </p>
+  </div>
   <div class="container-edit-delete-button">
     <button class="edit-button">Editer</button>
     <button class="delete-button">Supprimer</button>
@@ -84,10 +68,46 @@ const createTodoElement = (todo, index) => {
   faRegular.addEventListener("click", (event) => {
     toggleTodo(index);
   });
+  const editButton = li.querySelector(".edit-button");
+  editButton.addEventListener("click", (event) => {
+    event.stopPropagation();
+    toggleEditMode(index);
+  });
   const deleteButton = li.querySelector(".delete-button");
   deleteButton.addEventListener("click", (event) => {
     event.stopPropagation();
     deleteTodo(index);
+  });
+  return li;
+};
+
+const createTodoEditElement = (todo, index) => {
+  const li = document.createElement("li");
+  li.innerHTML = `
+  <fieldset class="container-edit-todo">
+  <legend class="edit-title-todo">
+    Ma tâche n°<span class="number">${index + 1}</span> :
+  </legend>
+  <div class="container-edit-text-todo">
+    <span class="todo">${index + 1}</span> 
+    <input type="text" />
+  </div>
+  <div class="container-save-cancel-button">
+    <button class="save-button">Sauvegarder</button>
+    <button class="cancel-button">Annuler</button>
+  </div>
+</fieldset>
+  `;
+  const input = li.querySelector("input");
+  input.value = todo.text;
+  const saveButton = li.querySelector(".save-button");
+  saveButton.addEventListener("click", (event) => {
+    editTodo(index, input);
+  });
+  const cancelButton = li.querySelector(".cancel-button");
+  cancelButton.addEventListener("click", (event) => {
+    event.stopPropagation();
+    toggleEditMode(index);
   });
   return li;
 };
@@ -109,6 +129,18 @@ const deleteTodo = (index) => {
 const toggleTodo = (index) => {
   todos[index].done = !todos[index].done;
   todos[index].check = !todos[index].check;
+  displayTodo();
+};
+
+const toggleEditMode = (index) => {
+  todos[index].editMode = !todos[index].editMode;
+  displayTodo();
+};
+
+const editTodo = (index, input) => {
+  const value = input.value;
+  todos[index].text = value;
+  todos[index].editMode = false;
   displayTodo();
 };
 
